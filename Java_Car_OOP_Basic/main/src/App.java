d803883943876f1ff5ada3497219606e756644ed
Arshad9633 <arshadk000@gmail.com>
2023-12-19 11:09:07 +0100

    First commit

---
+++ Java_Car_OOP_Basic/main/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args){
+        // Assuming we are creating a manual SUV for this example
+        SUV fortuner = new SUV("Fortuner", false);
+
+        fortuner.printTransmissionType();
+        fortuner.move(20, 0);
+        fortuner.accelerate(10);
+
+        System.out.println("Current Gear: " + fortuner.getCurrentGear());
+        System.out.println("Current Direction: " + fortuner.getCurrentDirection());
+    }
+}
---
+++ Java_Car_OOP_Basic/main/src/vehicle.java
@@ -0,0 +1,44 @@
+public class vehicle {
+    
+    private String name;
+    private int currentSpeed;
+    private int currentDirection;
+    public vehicle(String name) {
+        this.name = name;
+        this.currentSpeed = 0;
+        this.currentDirection = 0;
+    }
+    public void steer(int direction){
+        this.currentDirection += direction;
+        System.out.println("steer method: Steering at "+ currentDirection+" degrees");
+    }
+
+    public void move(int speed, int direction){
+        currentSpeed = speed; 
+        currentDirection = direction;
+        System.out.println("Move method called: Moveing at "+currentSpeed +" in direction" + currentDirection);
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getCurrentSpeed() {
+        return currentSpeed;
+    }
+    public void setCurrentSpeed(int currentSpeed) {
+        this.currentSpeed = currentSpeed;
+    }
+    public int getCurrentDirection() {
+        return currentDirection;
+    }
+    public void setCurrentDirection(int currentDirection) {
+        this.currentDirection = currentDirection;
+    }
+    public void stop(){
+        this.currentSpeed = 0;
+        System.out.println("Vechicle is stopped");
+    }
+    
+}
